c12afe1df0a2718360c0c80598e7570c2ca8e910
Gleb Mazovetskiy <glex.spb@gmail.com>
2019-11-15 16:14:11 +0000

    Update GCC and defconfig
    Also updates bison and flex
    Removes cloog

--- package/bison/bison.mk
+++ package/bison/bison.mk
@@ -4,20 +4,13 @@
 #
 ################################################################################
 
-BISON_VERSION = 3.0.4
+BISON_VERSION = 3.4.2
 BISON_SOURCE = bison-$(BISON_VERSION).tar.xz
 BISON_SITE = $(BR2_GNU_MIRROR)/bison
-BISON_LICENSE = GPLv3+
+BISON_LICENSE = GPL-3.0+
 BISON_LICENSE_FILES = COPYING
-BISON_CONF_ENV = ac_cv_path_M4=/usr/bin/m4
-BISON_DEPENDENCIES = m4
-BISON_AUTORECONF = YES
+# parallel build issue in examples/c/reccalc/
+BISON_MAKE = $(MAKE1)
+HOST_BISON_DEPENDENCIES = host-m4
 
-define BISON_DISABLE_EXAMPLES
-	echo 'all install:' > $(@D)/examples/Makefile
-endef
-
-BISON_POST_CONFIGURE_HOOKS += BISON_DISABLE_EXAMPLES
-
-$(eval $(autotools-package))
 $(eval $(host-autotools-package))
