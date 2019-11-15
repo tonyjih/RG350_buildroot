bafc1f0f2ed41e84c2c29b6481f8c34e1e1a5f1c
Gleb Mazovetskiy <glex.spb@gmail.com>
2019-11-15 12:43:48 +0000

    Upgrade python2+packages from upstream

--- package/python-pycrypto/python-pycrypto.mk
+++ package/python-pycrypto/python-pycrypto.mk
@@ -4,14 +4,15 @@
 #
 ################################################################################
 
-PYTHON_PYCRYPTO_VERSION = 2.6
+PYTHON_PYCRYPTO_VERSION = 2.6.1
 PYTHON_PYCRYPTO_SOURCE = pycrypto-$(PYTHON_PYCRYPTO_VERSION).tar.gz
 PYTHON_PYCRYPTO_SITE = http://ftp.dlitz.net/pub/dlitz/crypto/pycrypto
 PYTHON_PYCRYPTO_SETUP_TYPE = distutils
 
 PYTHON_PYCRYPTO_LICENSE = Public Domain, Python 2.2 License (HMAC.py, setup.py)
-PYTHON_PYCRYPTO_LICENSE_FILES = COPYRIGHT LEGAL/copy/LICENSE.libtom \
-		LEGAL/copy/LICENSE.python-2.2
+PYTHON_PYCRYPTO_LICENSE_FILES = \
+	COPYRIGHT LEGAL/copy/LICENSE.libtom \
+	LEGAL/copy/LICENSE.python-2.2
 
 # The pycrypto package contains a LICENSE.orig file, but our patching
 # infrastrucure removes all .orig file, so we must rename that license
@@ -21,9 +22,11 @@ define PYTHON_PYCRYPTO_RENAME_LICENSE
 	mv $(@D)/LEGAL/copy/LICENSE.orig $(@D)/LEGAL/copy/LICENSE.original
 endef
 PYTHON_PYCRYPTO_POST_EXTRACT_HOOKS += PYTHON_PYCRYPTO_RENAME_LICENSE
+HOST_PYTHON_PYCRYPTO_POST_EXTRACT_HOOKS += PYTHON_PYCRYPTO_RENAME_LICENSE
 PYTHON_PYCRYPTO_LICENSE_FILES += LEGAL/copy/LICENSE.original
 
 PYTHON_PYCRYPTO_DEPENDENCIES = gmp
+HOST_PYTHON_PYCRYPTO_DEPENDENCIES = host-gmp
 
 # The configure step needs to be run outside of the setup.py since it isn't
 # run correctly for cross-compiling
@@ -43,3 +46,4 @@ define PYTHON_PYCRYPTO_CONFIGURE_CMDS
 endef
 
 $(eval $(python-package))
+$(eval $(host-python-package))
