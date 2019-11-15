eef0ebc77d938951a189828ece89ac0b6f40f05c
Gleb Mazovetskiy <glex.spb@gmail.com>
2019-11-15 18:17:16 +0000

    Update package/apitrace from upstream

--- package/apitrace/apitrace.mk
+++ package/apitrace/apitrace.mk
@@ -4,21 +4,14 @@
 #
 ################################################################################
 
-APITRACE_VERSION = 5.0
+APITRACE_VERSION = 8.0
 APITRACE_SITE = $(call github,apitrace,apitrace,$(APITRACE_VERSION))
 APITRACE_LICENSE = MIT
 APITRACE_LICENSE_FILES = LICENSE
 
-APITRACE_DEPENDENCIES = popt binutils host-python
+APITRACE_DEPENDENCIES = xlib_libX11 host-python libpng
 
 # Gui was never tested, so we prefer to explicitly disable it
 APITRACE_CONF_OPTS += -DENABLE_GUI=false
-
-APITRACE_CONF_OPTS = -DPYTHON_EXECUTABLE:STRING="$(HOST_DIR)/usr/bin/python"
-
-ifneq ($(BR2_PACKAGE_XLIB_LIBX11),y)
-APITRACE_CONF_OPTS += -DCMAKE_C_FLAGS:STRING="$(TARGET_CFLAGS) -DMESA_EGL_NO_X11_HEADERS"
-APITRACE_CONF_OPTS += -DCMAKE_CXX_FLAGS:STRING="$(TARGET_CFLAGS) -DMESA_EGL_NO_X11_HEADERS"
-endif
 
 $(eval $(cmake-package))
